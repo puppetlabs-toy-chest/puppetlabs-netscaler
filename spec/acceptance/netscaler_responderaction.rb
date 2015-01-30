25c76043d2f0d28ca3f24f37343551dacc155d9b
tphoney <tp@puppetlabs.com>
2015-01-30 16:29:20 +0000

    adding in acceptance tests for responderaction

---
+++ spec/acceptance/netscaler_responderaction.rb
@@ -0,0 +1,17 @@
+require 'spec_helper_acceptance'
+
+describe 'responderaction' do
+  it 'makes a responderaction' do
+    pp=<<-EOS
+netscaler_responderaction { 'monkey':
+  ensure   => 'present',
+  comments => 'banana',
+  target   => 'bla',
+  type     => 'sqlresponse_ok',
+}
+    EOS
+    make_site_pp(pp)
+    run_device(:allow_changes => true)
+    run_device(:allow_changes => false)
+  end
+end
