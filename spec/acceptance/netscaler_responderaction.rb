e4966e217ed688d30bc193620fc4977141456ac5
tphoney <tp@puppetlabs.com>
2015-02-04 14:26:47 +0000

    better testing, ignore .folders

--- spec/acceptance/netscaler_responderaction.rb
+++ spec/acceptance/netscaler_responderaction.rb
@@ -15,4 +15,56 @@ netscaler_responderaction { 'monkey':
     run_device(:allow_changes => true)
     run_device(:allow_changes => false)
   end
+
+  it 'edit a responderaction' do
+    pp=<<-EOS
+netscaler_responderaction { 'edit':
+  ensure            => 'present',
+  comments          => 'first',
+  target            => 'first',
+  type              => 'sqlresponse_ok',
+  bypasssafetycheck => 'YES',
+}
+    EOS
+    make_site_pp(pp)
+    run_device(:allow_changes => true)
+    run_device(:allow_changes => false)
+
+    pp2=<<-EOS
+netscaler_responderaction { 'edit':
+  ensure            => 'present',
+  comments          => 'second',
+  target            => 'second',
+  bypasssafetycheck => 'NO',
+}
+    EOS
+    make_site_pp(pp2)
+    run_device(:allow_changes => true)
+    run_device(:allow_changes => false)
+  end
+
+ it 'delete a responderaction' do
+    pp=<<-EOS
+netscaler_responderaction { 'delete':
+  ensure            => 'present',
+  comments          => 'first',
+  target            => 'first',
+  type              => 'sqlresponse_ok',
+  bypasssafetycheck => 'YES',
+}
+    EOS
+    make_site_pp(pp)
+    run_device(:allow_changes => true)
+    run_device(:allow_changes => false)
+
+    pp2=<<-EOS
+netscaler_responderaction { 'edit':
+  ensure            => 'absent',
+}
+    EOS
+    make_site_pp(pp2)
+    run_device(:allow_changes => true)
+    run_device(:allow_changes => false)
+  end
+
 end
