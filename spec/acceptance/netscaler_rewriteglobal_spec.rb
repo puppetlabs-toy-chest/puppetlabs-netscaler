47a58afafed6d38c8a0422bcaf4924c4f1f01bd9
Jonathan Tripathy <jt@puppetlabs.com>
2015-02-17 12:21:48 +0000

    Implemented new way of handling req/resvserver for rewrite global

--- spec/acceptance/netscaler_rewriteglobal_spec.rb
+++ spec/acceptance/netscaler_rewriteglobal_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper_acceptance'
 
 describe 'rewriteglobal' do
-  it 'makes a rewriteglobal' do
+  it 'makes a rewriteglobal (reqvserver)' do
     pp=<<-EOS
 netscaler_lbvserver { 'lbvirtualserver1':
   ensure       => 'present',
@@ -24,6 +24,7 @@ netscaler_rewriteglobal {'rewritepolicy_test1':
   priority             => '100',
   goto_expression      => 'END',
   invoke_vserver_label => 'lbvirtualserver1',
+  connection_type      => 'Request',
 }
     EOS
     make_site_pp(pp)
@@ -31,7 +32,7 @@ netscaler_rewriteglobal {'rewritepolicy_test1':
     run_device(:allow_changes => false)
   end
 
- it 'delete a rewriteglobal' do
+ it 'delete a rewriteglobal (reqvserver)' do
     pp=<<-EOS
 netscaler_lbvserver { 'lbvirtualserver2':
   ensure       => 'present',
@@ -54,6 +55,7 @@ netscaler_rewriteglobal {'rewritepolicy_test2':
   priority             => '101',
   goto_expression      => 'END',
   invoke_vserver_label => 'lbvirtualserver2',
+  connection_type      => 'Request',
 }
     EOS
     make_site_pp(pp)
@@ -66,6 +68,58 @@ netscaler_rewriteglobal { 'rewritepolicy_test2':
 }
     EOS
     make_site_pp(pp2)
+    run_device(:allow_changes => true)
+    run_device(:allow_changes => false)
+  end
+
+  it 'makes a rewriteglobal (resvserver)' do
+    pp=<<-EOS
+netscaler_lbvserver { 'lbvirtualserver3':
+  ensure       => 'present',
+  ip_address   => '10.0.0.3',
+  port         => '80',
+  service_type => 'HTTP',
+  state        => 'ENABLED',
+}
+
+netscaler_rewritepolicy { 'rewritepolicy_test3':
+  ensure                  => 'present',
+  action                  => 'NOREWRITE',
+  comments                => 'comment',
+  expression              => 'HTTP.RES.HEADER("test").CONTAINS("test")',
+  undefined_result_action => 'DROP',
+}
+
+netscaler_rewriteglobal {'rewritepolicy_test3':
+  ensure               => 'present',
+  priority             => '102',
+  goto_expression      => 'END',
+  invoke_vserver_label => 'lbvirtualserver3',
+  connection_type      => 'Response',
+}
+    EOS
+    make_site_pp(pp)
+    run_device(:allow_changes => true)
+    run_device(:allow_changes => false)
+  end
+
+  it 'makes a rewriteglobal (no invoke)' do
+    pp=<<-EOS
+netscaler_rewritepolicy { 'rewritepolicy_test4':
+  ensure                  => 'present',
+  action                  => 'NOREWRITE',
+  comments                => 'comment',
+  expression              => 'HTTP.RES.HEADER("test").CONTAINS("test")',
+  undefined_result_action => 'DROP',
+}
+
+netscaler_rewriteglobal {'rewritepolicy_test4':
+  ensure               => 'present',
+  priority             => '103',
+  goto_expression      => 'END',
+}
+    EOS
+    make_site_pp(pp)
     run_device(:allow_changes => true)
     run_device(:allow_changes => false)
   end
