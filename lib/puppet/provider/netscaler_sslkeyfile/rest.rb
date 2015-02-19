7205b8bbbc008b7151aa405b5568bad62dc7274b
tphoney <tp@puppetlabs.com>
2015-02-19 17:34:08 +0000

    adding in ssl types and providers

---
+++ lib/puppet/provider/netscaler_sslkeyfile/rest.rb
@@ -0,0 +1,53 @@
+require 'puppet/provider/netscaler'
+require 'json'
+
+Puppet::Type.type(:netscaler_sslkeyfile).provide(:rest, parent: Puppet::Provider::Netscaler) do
+  def netscaler_api_type
+    "sslkeyfile"
+  end
+
+  def self.instances
+    instances = []
+    sslkeyfiles = Puppet::Provider::Netscaler.call('/config/sslkeyfile')
+    #array can contain an empty hash, if there are no files uploaded
+    return [] if sslkeyfiles.nil? || sslkeyfiles[0].empty?
+
+    sslkeyfiles.each do |sslkeyfile|
+      instances << new(
+        :ensure   => :present,
+        :name     => sslkeyfile['name'],
+        :source   => sslkeyfile['src'],
+      )
+    end
+
+    instances
+  end
+
+  mk_resource_methods
+
+  # Map for conversion in the message.
+  def property_to_rest_mapping
+    {
+      :source => :src,
+    }
+  end
+
+  def immutable_properties
+    [
+      :name,
+      :source,
+    ]
+  end
+
+  def create
+    @create_elements = true
+    result = Puppet::Provider::Netscaler.post("/config/#{netscaler_api_type}", message(resource), {"action" => "import"})
+    @property_hash.clear
+ 
+    return result
+  end
+
+  def per_provider_munge(message)
+    message
+  end
+end
