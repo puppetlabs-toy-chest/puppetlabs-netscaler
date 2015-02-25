126c06f573a3957a9814484fa4db4a245663a6e5
Jonathan Tripathy <jt@puppetlabs.com>
2015-02-25 14:38:14 +0000

    Additional test for lbvserver_responderpolicy_bind

--- spec/acceptance/netscaler_lbvserver_responderpolicy_bind_spec.rb
+++ spec/acceptance/netscaler_lbvserver_responderpolicy_bind_spec.rb
@@ -65,4 +65,41 @@ describe 'lbvserver-responderpolicy-binding' do
     run_device(:allow_changes => true)
     run_device(:allow_changes => false)
   end
+
+    it 'makes a lbvserver-responderpolicy-binding (resvserver)' do
+    pp=<<-EOS
+netscaler_responderpolicy { '1_9_responderpolicy_test3':
+  ensure                  => 'present',
+  action                  => 'NOOP',
+  comments                => 'comment',
+  expression              => 'ANALYTICS.STREAM("Top_CLIENTS").COLLECT_STATS',
+  undefined_result_action => 'NOOP',
+}
+
+netscaler_lbvserver { '1_9_lbvserver_test3':
+  ensure        => 'present',
+  service_type  => 'HTTP',
+  state         => true,
+  ip_address    => '1.9.3.1',
+  port          => '8080',
+}
+
+netscaler_csvserver { '1_9_csvserver_test3':
+  ensure        => 'present',
+  service_type  => 'HTTP',
+  state         => true,
+  ip_address    => '1.9.3.2',
+  port          => '8080',
+}
+
+netscaler_lbvserver_responderpolicy_bind { '1_9_lbvserver_test3/1_9_responderpolicy_test3':
+  ensure               => present,
+  priority             => 1,
+  invoke_vserver_label => '1_9_csvserver_test3',
+}
+EOS
+    make_site_pp(pp)
+    run_device(:allow_changes => true)
+    run_device(:allow_changes => false)
+  end
 end
