50cb08e71d7d2f3fa3137db799baf79835d2863c
Jonathan Tripathy <jt@puppetlabs.com>
2015-02-27 15:09:49 +0000

    Code and test for csverver state changing

--- spec/acceptance/netscaler_csvserver_spec.rb
+++ spec/acceptance/netscaler_csvserver_spec.rb
@@ -68,4 +68,44 @@ describe 'csvserver tests' do
     run_device(:allow_changes => false)
   end
 
+  it 'makes and disables/enables a csvserver' do
+    pp=<<-EOS
+    netscaler_csvserver { '2_1_csvserver4':
+      ensure       => 'present',
+      service_type => 'HTTP',
+      ip_address   => '2.1.4.1',
+      port         => '8080',
+      state        => 'ENABLED',
+    }
+    EOS
+
+    pp2=<<-EOS
+    netscaler_csvserver { '2_1_csvserver4':
+      ensure       => 'present',
+      service_type => 'HTTP',
+      ip_address   => '2.1.4.1',
+      port         => '8080',
+      state        => 'DISABLED',
+    }
+    EOS
+
+    pp3=<<-EOS
+    netscaler_csvserver { '2_1_csvserver4':
+      ensure       => 'present',
+      service_type => 'HTTP',
+      ip_address   => '2.1.4.1',
+      port         => '8080',
+      state        => 'ENABLED',
+    }
+    EOS
+    make_site_pp(pp)
+    run_device(:allow_changes => true)
+    make_site_pp(pp2)
+    run_device(:allow_changes => true)
+    run_device(:allow_changes => false)
+    make_site_pp(pp3)
+    run_device(:allow_changes => true)
+    run_device(:allow_changes => false)
+  end
+
 end
