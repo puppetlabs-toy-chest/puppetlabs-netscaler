ab9f5765fa1d785ae69abab66f7a2fa35f74e16a
Jonathan Tripathy <jt@puppetlabs.com>
2015-02-27 12:22:15 +0000

    Test and code for lbvserver state change.

--- spec/acceptance/netscaler_lbvserver_spec.rb
+++ spec/acceptance/netscaler_lbvserver_spec.rb
@@ -66,4 +66,47 @@ describe 'lbvserver tests' do
     run_device(:allow_changes => false)
   end
 
+  it 'makes and disables/enables a lbvserver' do
+    pp=<<-EOS
+    netscaler_lbvserver { '1_8_lbvserver4':
+      ensure       => 'present',
+      service_type => 'HTTP',
+      ip_address   => '1.8.4.1',
+      port         => '8080',
+      state        => 'ENABLED',
+    }
+    EOS
+    make_site_pp(pp)
+    run_device(:allow_changes => true)
+    run_device(:allow_changes => false)
+
+    pp2=<<-EOS
+    netscaler_lbvserver { '1_8_lbvserver4':
+      ensure       => 'present',
+      service_type => 'HTTP',
+      ip_address   => '1.8.4.1',
+      port         => '8080',
+      state        => 'DISABLED',
+    }
+    EOS
+
+    pp3=<<-EOS
+    netscaler_lbvserver { '1_8_lbvserver4':
+      ensure       => 'present',
+      service_type => 'HTTP',
+      ip_address   => '1.8.4.1',
+      port         => '8080',
+      state        => 'ENABLED',
+    }
+    EOS
+    make_site_pp(pp)
+    run_device(:allow_changes => true)
+    make_site_pp(pp2)
+    run_device(:allow_changes => true)
+    run_device(:allow_changes => false)
+    make_site_pp(pp3)
+    run_device(:allow_changes => true)
+    run_device(:allow_changes => false)
+  end
+
 end
