fb50c240e99e8cab6fc7c41ed8242039cbcc41df
tphoney <tp@puppetlabs.com>
2016-01-20 14:25:49 +0000

    puppet 4 and greater now uses puppet_gem provider

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,9 +1,15 @@
 # Private class
 class netscaler::install {
-  $provider = $::puppetversion ? {
-    /Puppet Enterprise/ => 'pe_gem',
-    default             => 'gem',
+  if versioncmp('4', $::puppetversion) > 0 {
+    $provider = $::puppetversion ? {
+      /Puppet Enterprise/ => 'pe_gem',
+      default             => 'gem',
+    }
+  } else {
+    #everything later than 4 use puppet_gem
+    $provider  = 'puppet_gem'
   }
+  notice ("provider: ${$provider}")
   if ! defined(Package['faraday']) {
     package { 'faraday':
       ensure   => present,
