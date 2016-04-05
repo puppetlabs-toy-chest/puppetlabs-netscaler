44d0770949ce13ecd8e75a20747683bf494c97a3
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2016-04-05 10:37:46 +0200

    Rename sslvserver to sslvserver_sslcertkey_binding
    The `sslvserver` is something else, and I also have
    `sslvserver_ecccurve_binding` and `sslvserver_sslcipher_binding` coming
    up.
    
    This change is not backward compatible - you *will have* to change
    `netscaler_sslvserver` to `netscaler_sslvserver_sslcertkey_binding` ...

--- lib/puppet/type/netscaler_sslvserver_sslcertkey_binding.rb
+++ lib/puppet/type/netscaler_sslvserver_sslcertkey_binding.rb
@@ -2,14 +2,14 @@ require_relative('../../puppet/parameter/netscaler_name')
 require_relative('../../puppet/property/netscaler_traffic_domain')
 require_relative('../../puppet/property/netscaler_truthy')
 
-Puppet::Type.newtype(:netscaler_sslvserver) do
-  @doc = 'Configuration for SSL virtual server resource.'
+Puppet::Type.newtype(:netscaler_sslvserver_sslcertkey_binding) do
+  @doc = 'Configuration for SSL virtual server resource - binding with certkey.'
 
   apply_to_device
   ensurable
 
   newparam(:name, :parent => Puppet::Parameter::NetscalerName, :namevar => true)
-  desc "This is the binding of sslvserver/certkeyname"
+  desc "This is the binding of sslvserver/certkey"
   #XXX Validate with the below
   #ensure: change from absent to present failed: Could not set 'present' on ensure: REST failure: HTTP status code 400 detected.  Body of failure is: { "errorcode": 1075, "message": "Invalid name; names must begin with an alphanumeric character or underscore and must contain only alphanumerics, '_', '#', '.', ' ', ':', '@', '=' or '-' [name, hunner's website]", "severity": "ERROR" } at 55:/etc/puppetlabs/puppet/environments/produc
 
@@ -17,7 +17,7 @@ Puppet::Type.newtype(:netscaler_sslvserver) do
     desc "The state of the CRL check parameter. (Mandatory/Optional).
 Possible values = Mandatory, Optional"
   end
-  
+
   newproperty(:ca) do
     desc "CA certificate."
   end
